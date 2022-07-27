8bf9e8528db736b4fbda5e5af610d79091a9a4af
Jon <salhusj96@gmail.com>
2022-07-27 04:45:08 +0000

    adds helloworld.sh to repo

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Jon Salhus                      
+# Date of latest revision:  7/26/2022    
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "I am working perfectly fine"
+# End
